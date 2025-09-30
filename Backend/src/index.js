647db064246bc764e7e8a9513428fb51ebb33178
Sameer Mandve <mandvesameer@gmail.com>
2025-09-30 20:40:15 +0530

    Refactor path resolution in index.js and correct casing in file path

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,5 @@
 import dotenv from "dotenv";
 import path from "path";
-import { fileURLToPath } from "url";
 import express from "express";
 
 // Configuration for env file
@@ -12,8 +11,7 @@ import { connectDB } from "./db/database.js";
 import { app } from "./app.js";
 import { setupSocket } from "./utils/socket.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 
 // Database connection
 connectDB()
@@ -24,7 +22,7 @@ connectDB()
             app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
             app.get(/.*/, (_, res) => {
-                res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+                res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
             });
         }
 
